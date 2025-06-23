d5c27960cd5e53edf17acef062b35fea74bf6c84
bill-twomey <108663106@umail.ucc.ie>
2025-06-23 21:16:08 +0100

    third commit

--- frontend/src/pages/apod.jsx
+++ frontend/src/pages/apod.jsx
@@ -3,63 +3,139 @@ import { useEffect,useState } from "react";
 const Apod =() => {
 
   const [NasaData,setNasaData] =useState(null);
-  const [searchDate,setsearchDate]=useState(null);
+  const [searchDate,setsearchDate]=useState("");
+  const [loading,setLoading] =useState(false)
+  const [searching,setSearching] =useState(false)
+  // const [imageLoaded,setimageLoaded] =useState(false)
+  const [nextData, setNextData] = useState(null); 
+  const [searchinputerror,setSearchInputError] =useState("");
+  const [errorMessage,seterrorMessage] =useState("")
+  
 
+
+  // receive apod from the backend 
   useEffect(()=> {
   
+  
     const fetchdata = async ()=> {
+      setLoading(true)
     try {
     const res =await fetch('http://localhost:3000/api/apod',{mode:"cors"});
     const data =await res.json();
-    setNasaData(data);}
+    setNasaData(data)
+    setLoading(false)}
     catch(error){
-      console.error(error)}
+      console.error(error);
+    // setSearching(false)
+    }
 
   }
   fetchdata();
 },[]);
 
+// set searchDate with user input
 const userSearch =(event) => {
   setsearchDate(event.target.value)
 }
 
+// connect to the backend with user search date
+// no response needed ??? - just sending
 const submitSearch =async () => {
-  const response = await fetch('http://localhost:3000/api/usersearch', 
-                    {headers: { 'Content-Type': 'application/json' },
-                    body:JSON.stringify({searchDate})}
-  )
+  if (!searchDate) {
+    setSearchInputError("Please enter a search date")
+    return;
+  }
+  setSearchInputError("")
+  seterrorMessage("")
+  setSearching(true);
 
-  const data =await response.json();
-  setNasaData(data)
 
+  try {
+  const response = await fetch('http://localhost:3000/api/getSearchDate', 
+                    {method: "POST",
+                      headers: { 'Content-Type': 'application/json' },
+                    body:JSON.stringify({searchDate})} )
+                  const recd =  await response.json()
+                if (recd) {
+                     
+    
+    const res =await fetch('http://localhost:3000/api/apodSearchDate',{mode:"cors"});
+      if (!res.ok) {
+        let errorMessage ="something went wrong"
+        setSearching(false)
+
+      try {
+    const errordata =await res.json();
+    errorMessage=  `Error ${errordata.status} - ${errordata.message}`
+    // throw new Error(errordata.message); 
+  } catch {
+    errorMessage=`Server returned ${res.status}`
+
+  }throw new Error(errorMessage); 
+  
 }
+    const data =await res.json();
+    // setNasaData(data);
+    setNextData(data) 
+  }
+}
+    catch(error){
+      console.error(error.message)
+      seterrorMessage(error.message)
 
-
-
+  } 
+ } 
+        
+  if (loading) {
+    return <p>loading...</p>
+  }
+  else {
   return (
     <div className="bg-black min-h-screen text-white p-8 ">
-    <div className="max-w-4xl mx-auto space-y-6 flex flex-col">
+    <div className="max-w-6xl mx-auto space-y-6 flex flex-col">
 
       <h2 className="text-4xl font-bold text-center ">Astronomy Picture of the Day Apod</h2>
       <label className="">
-      <input  value={searchDate} onChange={userSearch()} className="border-white bg-white text-black" type ="search" name="search" placeholder="input a date"/>
-      <button onClick={submitSearch()} type="submit">Search</button>
+      <input  value={searchDate} onChange={userSearch} className="border-white bg-white text-black p-1" type ="date" name="search" placeholder="input a date"/>
+      <button className="ml-3 bg-blue-900 rounded p-1 px-2" onClick={submitSearch} type="submit">Search</button>
       </label>
+      {searching && (
+        <span>Searching.... </span>
+      )}
+      {searchinputerror && <p>{searchinputerror}</p>}
+      {errorMessage && <p>{errorMessage}</p>}
 
  
     <main className="">
-     <div className="text-center">
+
+      {nextData && (
+         <img src={nextData?.url} 
+         alt="" 
+         style={{ display: 'none' }}
+         onLoad={()=> {setSearching(false); setNasaData(nextData); setNextData(null)}} />
+        
+      ) }
+
+
+
+
+      {NasaData &&(
+      <div className="flex flex-row justify-items" >
+     <div className="text-center w-2/3">
       <p>{NasaData?.title}</p>
-      <img className="max-h-[450px] w-auto object-contain mx-auto"src={NasaData?.url} alt="image" />
+      <img className="max-h-[450px] w-auto object-contain mx-auto"src={NasaData?.url} alt="image"  />
      </div>
+     <div className="w-1/3">
       <p>{NasaData?.explanation}</p>
+      </div>
 
-
+     </div>
+     )}
     </main>
     </div>
     </div>
   );
 };
-
+}
 
 export default Apod;
--- frontend/src/pages/asteroid.jsx
+++ frontend/src/pages/asteroid.jsx
@@ -6,35 +6,59 @@ const Asteroid=() => {
 
     const [AsteroidData,setAsteroidData] =useState([])
     const [highlight,setHighlight]=useState(null)
-    // const [highlightAst,setHighlightAst]=useState(null)
+    const [loading,setLoading] = useState(false)
+    // default option
+    const [asteroidFilter,setasteroidFilter] =useState("closest")
+     const [errorMessage,seterrorMessage] =useState("")
   
     
-
+   // fetch asteroid data
     useEffect (() => {
-
+        // const params={asteroidFilter}
         const fetchdata = async ()=> {
+            setLoading(true)
+           
             try {
-                const res=await fetch('http://localhost:3000/api/asteroid',{mode:"cors"});
+                
+                const res=await fetch(`http://localhost:3000/api/asteroid?filter=${asteroidFilter}`, { mode:"cors"});
+                console.log({asteroidFilter})
+                if (!res.ok) {
+                    throw new Error(`Server responded with ${res.status} ${res.statusText}`)
+                }
                 const data =await res.json();
                 setAsteroidData(data);
+                setLoading(false);
                 
             }
             catch(error) {
                 console.error(error)
+                seterrorMessage(error)
+            }
+            finally {
+                setLoading(false)
             }
         }
         fetchdata();
-    },[]
+    },[asteroidFilter]
     )
-    //    const highlightdata = () => {
-    //       setHighlight(true)
+    // send asteroid filter to backend
+       const filterData = (event) => {
+          setasteroidFilter(event.target.value)
 
-    //    }
+       }
+
+    if (loading) {
+        return <p>loading...</p>
+    }
+    if (errorMessage) {
+        return <p>{errorMessage.message}</p>
+    }
+ 
     
     return (
 
         <div className="bg-black min-h-screen text-white p-8 ">
-        <main className="max-w-4xl mx-auto space-y-6 flex flex-col">
+        <main className="max-w-6xl mx-auto space-y-6 flex flex-col">
         <h2 className="font-bold ">Asteroids</h2>
 
         <div className="relative h-[300px]">
@@ -44,7 +68,7 @@ const Asteroid=() => {
                 const scaled=(Math.round(values.distance*0.0001))
                 return (
                 <img key={index}
-                    className = {` absolute left-0 top-1/2 -translate-y-1/2 object-contain  ${highlight===index ? "scale-120":""}`}
+                    className = {` absolute left-0 top-1/2 -translate-y-1/2 object-contain  ${highlight===index ? "scale-125":""}`}
                     onMouseOver={()=>setHighlight(index)} 
                     onMouseLeave={()=>setHighlight(null)}
                     src={asteroid} 
@@ -56,33 +80,83 @@ const Asteroid=() => {
         </div>
 
         <div>
-        
-        {/* { AsteroidData.length>0 && AsteroidData.map((values,index)=>
-        <p key={index}> {values.name} {values.distance}</p> */}
-        <table>
-            <tbody>
-                { AsteroidData.length>0 && AsteroidData.map((values,index)=>
-                <tr key={index} 
-                    className={highlight === index ?"bg-white":""} 
-                    onMouseOver={()=>setHighlight(index)} 
-                    onMouseLeave={()=>setHighlight(null)}
-                >  
-                  <td className={highlight === index ?"text-black":""} > {values.name} </td>
-                  <td className={highlight === index ?"text-black":""} > {values.distance} </td>
-                </tr>
-            )}
-            </tbody>
-        </table>
-        
+            <label className="block mb-2">
+                Filter asteroids:
+            </label>
+                <select className="bg-white text-black "
+                        onChange={filterData}>
+                    <option value="closest">closest</option>
+                    <option value="largest">largest</option>
+                    <option value="hazardous">hazardous</option>
+                </select>
+            
         </div>
+
+        <div className="flex flex-row gap-4">
+        { AsteroidData.length>0 && AsteroidData.map((values,index)=>(
+        
+        <div key={index} className={`flex flex-col border border-slate-200 rounded-lg  w-full max-w-md  ${highlight === index ?"scale-110":""}`}
+                        onMouseOver={()=>setHighlight(index)} 
+                    onMouseLeave={()=>setHighlight(null)}
+        >
+            <div className={`bg-blue-900 text-center rounded-t `} >
+                <p>{values.name}</p>
+            </div>
+            <div className={`space-y-1 p-3`}
+
+            >
+                <p >Miss distance:{values.distance}</p>
+                <p >Miss date:{values.missdate}</p>
+                <p >Potentially hazardous: {values.hazardous}</p>
+                <p >Min Est Diameter: {values.minDiameter}</p>
+                <p >Max Est Diameter: {values.maxDiameter}</p>
+            </div>
+        </div>
+
+        
+         ))}
+        </div>
+     
        
         </main>
         </div>
 
-       
+        /* { AsteroidData.length>0 && AsteroidData.map((values,index)=>
+        <p key={index}> {values.name} {values.distance}</p> */
+        /* <table>
+            <thead>
+                <tr>
+                    <th className="p-1">Name</th>
+                    <th className="p-1">Miss distance</th>
+                    <th className="p-1">Miss date</th>
+                    <th className="p-1">Min Est Diameter</th>
+                    <th className="p-1">Max Est Diameter</th>
+                    <th className="p-1">Potentially Hazardous</th>
+                </tr>
+            </thead>
+            <tbody>
+                { AsteroidData.length>0 && AsteroidData.map((values,index)=>
+                <tr key={index} 
+                    className={`${highlight === index ?"bg-white":""} `}
+                    onMouseOver={()=>setHighlight(index)} 
+                    onMouseLeave={()=>setHighlight(null)}
+                >  
+                  <td className={`p-1 ${highlight === index ?"text-black":""}`} > {values.name} </td>
+                  <td className={`p-1 ${highlight === index ?"text-black":""}`}> {values.distance} </td>
+                  <td className={`p-1 ${highlight === index ?"text-black":""}`} > {values.missdate} </td>
+                  <td className={`p-1 ${highlight === index ?"text-black":""}`} > {values.minDiameter} </td>
+                  <td className={`p-1 ${highlight === index ?"text-black":""}`} > {values.maxDiameter} </td>
+                  <td className={`p-1 ${highlight === index ?"text-black":""}`} > {values.hazardous} </td>
+                </tr>
+            )}
+            </tbody>
+        </table>
+         */
 
     );
-
+    
 };
+
+
 
 export default Asteroid
